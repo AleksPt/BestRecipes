c903bf2b345d0ed3cc17cd91a9947ef8a55caa3a
Alex Petrenko <alex.pt@ya.ru>
2024-07-01 11:31:35 +0300

    add enum for colors

---
+++ best-recipes-ios/Helpers/Colors.swift
@@ -0,0 +1,66 @@
+//
+//  Colors.swift
+//  best-recipes-ios
+//
+//  Created by Алексей on 01.07.2024.
+//
+
+import UIKit
+
+enum Colors {
+    enum Error {
+        static let error10: UIColor = .error10
+        static let error100: UIColor = .error100
+    }
+    
+    enum Success {
+        static let success10: UIColor = .success10
+        static let success100: UIColor = .success100
+    }
+    
+    enum Rating {
+        static let rating100: UIColor = .rating100
+    }
+    
+    enum Neutral {
+        static let neutral0: UIColor = .neutral0
+        static let neutral10: UIColor = .neutral10
+        static let neutral20: UIColor = .neutral20
+        static let neutral30: UIColor = .neutral30
+        static let neutral40: UIColor = .neutral40
+        static let neutral50: UIColor = .neutral50
+        static let neutral60: UIColor = .neutral60
+        static let neutral70: UIColor = .neutral70
+        static let neutral80: UIColor = .neutral80
+        static let neutral90: UIColor = .neutral90
+        static let neutral100: UIColor = .neutral100
+    }
+    
+    enum Primary {
+        static let primary0: UIColor = .primary0
+        static let primary10: UIColor = .primary10
+        static let primary20: UIColor = .primary20
+        static let primary30: UIColor = .primary30
+        static let primary40: UIColor = .primary40
+        static let primary50: UIColor = .primary50
+        static let primary60: UIColor = .primary60
+        static let primary70: UIColor = .primary70
+        static let primary80: UIColor = .primary80
+        static let primary90: UIColor = .primary90
+        static let primary100: UIColor = .primary100
+    }
+    
+    enum Secondary {
+        static let secondary0: UIColor = .secondary0
+        static let secondary10: UIColor = .secondary10
+        static let secondary20: UIColor = .secondary20
+        static let secondary30: UIColor = .secondary30
+        static let secondary40: UIColor = .secondary40
+        static let secondary50: UIColor = .secondary50
+        static let secondary60: UIColor = .secondary60
+        static let secondary70: UIColor = .secondary70
+        static let secondary80: UIColor = .secondary80
+        static let secondary90: UIColor = .secondary90
+        static let secondary100: UIColor = .secondary100
+    }
+}
