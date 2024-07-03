e3d720d0f53cc01677887b892b125517311cd44e
Nodnet <nodnet@mail.ru>
2024-07-03 11:26:07 +0300

    correct feature Rating Factory

--- best-recipes-ios/Factories/RatingFactory.swift
+++ best-recipes-ios/Factories/RatingFactory.swift
@@ -11,18 +11,22 @@ final class RatingFactory {
     static func makeSavedTrandingRating(image:UIImageView, ratingLabel:String, blur:Bool = true) -> UIView {
         let ratingView = UIView(frame: CGRect(origin: .zero, size: CGSize(width: 58, height: 28)))
         ratingView.layer.cornerRadius = 10
+        
         let blurEffect = UIBlurEffect(style: .dark)
         let blurEffectView = UIVisualEffectView(effect: blurEffect)
         blurEffectView.layer.cornerRadius = 10
         blurEffectView.clipsToBounds = true
         blurEffectView.frame = ratingView.frame
+        
         var localImage = UIImageView(frame: CGRect(origin: .zero, size: CGSize(width: 12, height: 12)))
         localImage = image
         localImage.contentMode = .scaleAspectFit
+        
         let label = UILabel(frame: CGRect(x: 28, y: 3, width: 24, height: 20))
-        label.textColor = .neutral0
-        label.font = .boldSystemFont(ofSize: 14)
+        label.textColor = UIColor.Colors.Rating.rating100
+        label.font = UIFont.PoppinsFont.semibold(size: 14)
         label.text = ratingLabel
+        
         ratingView.addSubview(blurEffectView)
         blurEffectView.contentView.addSubview(localImage)
         blurEffectView.contentView.addSubview(label)
