be358ca0191a5a87a82f73d27e8e4ae3ec95d4ed
Elena Loginova <iosdevlogin@gmail.com>
2024-07-04 14:01:52 +0500

    Create DetailVC

---
+++ best-recipes-ios/RecipeDetailScreen/RecipeDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeDetailView.swift
+//  best-recipes-ios
+//
+//  Created by Елена Логинова on 04.07.2024.
+//
+
+import UIKit
+
+class RecipeDetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ best-recipes-ios/RecipeDetailScreen/RecipeDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RecipeDetailViewController.swift
+//  best-recipes-ios
+//
+//  Created by Елена Логинова on 04.07.2024.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
