7027792c688a975cb5b2fc4ed14c65273db79ce8
Elena Loginova <iosdevlogin@gmail.com>
2024-07-12 04:03:07 +0500

    Fix loadView

--- best-recipes-ios/RecipeDetailScreen/RecipeDetailViewController.swift
+++ best-recipes-ios/RecipeDetailScreen/RecipeDetailViewController.swift
@@ -26,6 +26,10 @@ class RecipeDetailViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func loadView() {
+        view = recipeDetailView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
